42d458bb30b5345d51fc12f5bb4da5e28f39ed31
masakk1 <igotscammedlmfaobutimmasak1@gmail.com>
2023-09-20 20:07:32 +0200

    StyLua'd!

--- main.lua
+++ main.lua
@@ -1,38 +1,35 @@
 --++ DEBUGGING ++--
 if arg[2] == "debug" then
-    require("lldebugger").start()
+	require("lldebugger").start()
 end
 --++ DEBUGGING ++--
 
 --// Load //
 function love.load()
-    Object = require "lib.classic"
-    require "classes.player"
+	Object = require("lib.classic")
+	require("classes.player")
 
-    player = Player()
+	player = Player()
 end
-
 
 --// Draw //
 function love.draw()
-    player:draw()
+	player:draw()
 end
-
 
 --// Update //
 function love.update(dt)
-    player:update(dt)
+	player:update(dt)
 end
-
 
 --++ DEBUGGING ++--
 local love_errorhandler = love.errhand
 
 function love.errorhandler(msg)
-    if lldebugger then
-        error(msg, 2)
-    else
-        return love_errorhandler(msg)
-    end
+	if lldebugger then
+		error(msg, 2)
+	else
+		return love_errorhandler(msg)
+	end
 end
 --++ DEBUGGING ++--
